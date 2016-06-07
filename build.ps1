0bbbc039a68b26dc068f3bb903c08eaa3360186d
Matt Houser <matt@eleven41.com>
2016-06-07 14:44:23 -0400

    Add /Regions and custom templates

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@ $jarfile = "swagger-codegen-cli-2.1.6.jar"
 
 (Get-Content skeddly-api.yaml).replace('@build', $env:appveyor_build_version) | Set-Content skeddly-api-versioned.yaml
 
-& java -jar $jarfile generate -i skeddly-api-versioned.yaml -l html -o generated\html
+& java -jar $jarfile generate -i skeddly-api-versioned.yaml -l html -t templates\html -o generated\html
 
 Write-Host $LASTEXITCODE
